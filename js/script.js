ee91bf7bee0941437e09aa880802882b74c13560
MishaSkroba <skroba.misha@gmail.com>
2022-03-21 21:48:44 +0300

    Add dynamic menu generation. Remove static menu.

--- js/script.js
+++ js/script.js
@@ -133,15 +133,80 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const moadlTimerId = setTimeout(openModal, 100000);
+    const moadlTimerId = setTimeout(openModal, 10000);
 
     function showModalByScroll() {
         if(window.pageYOffset /*количество пикселей, на которое прокручен документ по вертикали*/ + document.documentElement.clientHeight >= document.documentElement.scrollHeight) { //document.documentElement возвращает элемент Element , который является коренным элементом документа  document
         openModal();
 
-        window.removeEventListener('scroll', showModalByScroll); //
+        window.removeEventListener('scroll', showModalByScroll); //Для того чтоб модаьлное окно показывалось молько 1 раз мы удаляем обработчик событий после сразу после вызова функции
         }
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+    
+    // Используем классы для создание карточек меню
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 9;
+            this.changeToUAH();
+        }
+
+        changeToUAH () {
+            this.price = this.transfer * this.price;
+        }
+
+        render () {
+            const div = document.createElement('div');
+
+            div.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>
+            `;
+
+            this.parent .append(div);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg", 
+        "vegy", 
+        'Меню "Фитнес"', 
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 
+        12, 
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container"
+    ).render();
 });
